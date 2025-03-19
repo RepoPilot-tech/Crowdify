567c3b1d5ab694eebea8c5f70cb608d79431f5c8
RepoPilot-tech <235473676+RepoPilot-tech@users.noreply.github.com>
2025-03-19 21:41:50 +0530

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export const config = {
 }
 
 export function middleware(request: NextRequest){
-    const authSessionToken = request.cookies.get('authjs.session-token') || request.cookies.get('next-auth.session-token');
+    const authSessionToken = request.cookies.get('authjs.session-token') || request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token');
 
     const url = request.nextUrl;
 
